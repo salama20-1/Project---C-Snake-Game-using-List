a5d4c3f1e41d25db34924073ba1f4e5b76cb1216
Nida Siddiqui <siddin25@mcmaster.ca>
2024-12-02 23:04:56 -0500

    commenting

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -3,101 +3,111 @@
 #include <iostream>
 
 using namespace std;
-// Check lecture contents on general purpose array list construction, 
-// and modify it to support objPos array list construction.
+// Check lecture contents on general purpose array list construction,
 
-objPosArrayList::objPosArrayList() // default
+// Initialize array
+objPosArrayList::objPosArrayList()
 {
     listSize = 0;
     arrayCapacity = ARRAY_MAX_CAP;
-    aList = new objPos[ARRAY_MAX_CAP];
+    aList = new objPos[arrayCapacity];
 }
-objPosArrayList::~objPosArrayList() // destructor
+
+objPosArrayList::~objPosArrayList()
 {
     delete[] aList;
 }
 
-objPosArrayList::objPosArrayList(const objPosArrayList &a) // copy
+objPosArrayList::objPosArrayList(const objPosArrayList &a)
 {
-
     listSize = a.listSize;
     arrayCapacity = a.arrayCapacity;
-
     aList = new objPos[arrayCapacity];
-    for(int i=0;i<listSize;i++)
+
+    for (int i = 0; i < listSize; ++i)
     {
         aList[i] = a.aList[i];
     }
 }
-objPosArrayList& objPosArrayList::operator = (const objPosArrayList &a) // copy assigment
+
+
+objPosArrayList& objPosArrayList::operator=(const objPosArrayList &a)
 {
-    if (this != &a)
+    if (this != &a) // Prevent self-assignment
     {
         delete[] aList;
         listSize = a.listSize;
         arrayCapacity = a.arrayCapacity;
-
         aList = new objPos[arrayCapacity];
 
-        for(int i=0;i<listSize;i++)
+        for (int i = 0; i < listSize; ++i)
         {
             aList[i] = a.aList[i];
         }
     }
-
     return *this;
 }
 
+// size of array list
 int objPosArrayList::getSize()
 {
     return listSize;
 }
+
+
 void objPosArrayList::insertHead(objPos thisPos)
 {
-    
-    for(int i=listSize;i>0;--i)
+    // Shift all elements to the right by one
+    for (int i = listSize; i > 0; --i)
     {
-        aList[i] = aList[i-1];
+        aList[i] = aList[i - 1];
     }
-    aList[0] = thisPos;
+    aList[0] = thisPos;  // Insert the new element at the head
     ++listSize;
 }
+
+// Insert an element at the tail (end) of the list.
 void objPosArrayList::insertTail(objPos thisPos)
 {
-    aList[listSize] = thisPos;
+    aList[listSize] = thisPos;  // Insert the new element at the tail
     ++listSize;
 }
+
+
 void objPosArrayList::removeHead()
 {
-    
-    for(int i=0;i<listSize-1;i++)
+    // Shift all elements to the left by one
+    for (int i = 0; i < listSize - 1; ++i)
     {
-        aList[i] = aList[i+1];
+        aList[i] = aList[i + 1];
     }
-    --listSize;
+    --listSize;  // Get rid of first element
 }
+
+// Remove the tail (last) element from the list.
 void objPosArrayList::removeTail()
 {
-    
-    --listSize; // Lazy delete
+    --listSize;  // Get rid of last element
 }
 
-//Getters
-
+// Getter for the head element.
 objPos objPosArrayList::getHeadElement()
 {
     return aList[0];
 }
 
+// Getter for the tail element.
 objPos objPosArrayList::getTailElement()
 {
-    return aList[listSize-1];
+    return aList[listSize - 1];
 }
 
+// Getter for an element at a specific index.
 objPos objPosArrayList::getElement(int index)
 {
     return aList[index];
 }
+
 
 
 
